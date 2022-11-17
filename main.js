40ac750523536f06c8db805a8eadc66d5d70eaac
OsmarAlvarado <osmar.alvarado.dev@gmail.com>
2022-11-17 21:24:17 +0100

    class2

---
+++ main.js
@@ -0,0 +1,27 @@
+const all = document.querySelector('body')
+const nroNights = document.querySelector('#nigths')
+const viajeOviedo = document.querySelector('.city1')
+const viajeTokio = document.querySelector('.city2')
+const viajeMadrid = document.querySelector('.city3')
+const viajeBarcelona = document.querySelector('.city4')
+const calBt = document.querySelector('button')
+
+const hotel = document.querySelector('.hotel')
+const avion = document.querySelector('.avion')
+const coche = document.querySelector('.coche')
+const result = document.querySelector('.result')
+
+function costeHotel(noches) {
+
+}
+costeHotel(nroNights)
+
+
+
+calBt.onclick = () => {
+
+}
+
+
+console.log(viajeBarcelona);
+//console.log(all);
