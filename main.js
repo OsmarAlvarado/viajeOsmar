210090b257095797964ad95261cc82b452695f9d
OsmarAlvarado <osmar.alvarado.dev@gmail.com>
2022-11-18 17:16:44 +0100

    final dia

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
 const all = document.querySelector('body')
 const nroNights = document.querySelector('#nigths')
-const viajeOviedo = document.querySelector('.city1')
-const viajeTokio = document.querySelector('.city2')
-const viajeMadrid = document.querySelector('.city3')
-const viajeBarcelona = document.querySelector('.city4')
+const viajes = document.querySelectorAll('.city')
 const calBt = document.querySelector('button')
 
-const hotel = document.querySelector('.hotel')
-const avion = document.querySelector('.avion')
-const coche = document.querySelector('.coche')
-const result = document.querySelector('.result')
-
-function costeHotel(noches) {
-
-}
-costeHotel(nroNights)
+const inputHotel = document.querySelector('.hotel')
+const inputAvion = document.querySelector('.avion')
+const inputCoche = document.querySelector('.coche')
+const inputResult = document.querySelector('.result')
 
 
 
 calBt.onclick = () => {
 
+    const inputNoches = nroNights.value
+    inputHotel.texContent = inputNoches
+
+
+}
+
+function costeHotel(nroNights) {
+    if (nroNights >= 1) {
+        return nroNights * 140
+    } else {
+        return 140
+    }
+
 }
 
 
-console.log(viajeBarcelona);
-//console.log(all);
+function alquilerCoche(nroNights) {
+
+}
+
+function costeAvion(nroNights) {
+
+}
+
+costeHotel(nroNights)
+
+console.log(viajes);
+console.log(all);
